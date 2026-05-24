d7da1619e3def17475eaab3b58beae16343b79ce
CHOI EUN SU <choieunsu12001@naver.com>
2026-05-24 17:16:06 +0900

    Feat/fcm: FCM 웹 푸시 알림 기능 구현
    - Firebase 프로젝트 연동 및 .env 환경변수 보안 처리
    - 백그라운드 수신을 위한 서비스 워커(firebase-messaging-sw.js) 구축
    - 사용자 알림 권한 요청 및 고유 FCM 토큰 발급 유틸 함수 구현
    - Map.jsx 내 화면 진입 시 토큰 획득 및 포그라운드 리스너 연동
    
    - 추후 백엔드와 연동 시 src/utils/fcm.js 파일의 requestAndGetFCMToken 함수 안에 백엔드 API 주소 첨부하면 됩니다.

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// src/firebase/config.js
+import { initializeApp } from "firebase/app";
+
+//.env 파일에 적어둔 비밀키값들 호출
+export const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+export const app = initializeApp(firebaseConfig);
